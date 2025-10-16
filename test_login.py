b7d81ae3eeb70bcd25c19dc9f919546610ccac7a
Josue Diaz <josuediaz8@gmail.com>
2025-10-16 19:20:32 -0300

    Test de Login OK

---
+++ test_login.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By #para poder llamar por By a los elementos, es como un acceso rapido
+import time
+
+driver = webdriver.Chrome() #iniciamos una variable por navegador
+
+driver.get("https://www.saucedemo.com/") # abre la pagina web
+
+
+def login ():
+     
+     try:
+        
+        driver.find_element(By.ID, "user-name").send_keys("standard_user") #encuentro por ID y le escribo el nombre de usuario
+        
+        driver.find_element(By.NAME, "password").send_keys("secret_sauce") #encuentro por NAME y le escribo el nombre de usuario
+        
+        time.sleep(2) #espero 2 seg
+
+        boton_login = driver.find_element(By.XPATH, "//*[@id='login-button']") #encuentro por XPath
+        
+        boton_login.click() #hago click en el boton de login
+
+        time.sleep(3) #esperamos 3 segundos
+     
+     except Exception as e:
+         
+         print(f"Error al iniciar sesion: {e}") #en caso de error muestra msj por consola
+
+     finally:
+        driver.quit()
+
+login ()
