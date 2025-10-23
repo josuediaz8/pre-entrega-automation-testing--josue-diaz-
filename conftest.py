02c47371b35c1569ee1c157c7f7cadee228cc5bc
Josue Diaz <josuediaz8@gmail.com>
2025-10-23 15:25:32 -0300

    Modifico el Conftest para que abra Chrome en incognito y no salga ventana de contraseña

--- conftest.py
+++ conftest.py
@@ -1,12 +1,17 @@
 import pytest
 from selenium import webdriver
 from utils import login
+from selenium.webdriver.chrome.options import Options
+
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
-    yield driver #yield se usa con fixture, permite ejecutaqr la prueba en el medio / se pausa el codigo
+    chrome_opt = Options() 
+    chrome_opt.add_argument("--incognito") #abre chrome como incognito y desactiva ventana de contraseña
+    driver = webdriver.Chrome(options=chrome_opt)
+    yield driver
     driver.quit()
+
 
 @pytest.fixture
 def login_in_diver (driver):
